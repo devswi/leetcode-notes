2512a00d7f3182d7361b5420f35a225f79c48c3b
shiwei93 <stayfocusjs@gmail.com>
2022-09-18 17:27:12 +0800

    [Add] 0100 finished

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -1,3 +1,4 @@
 mod p0094_binary_tree_inorder_traversal;
+mod p0100_same_tree;
 mod p0144_binary_tree_preorder_traversal;
 mod p0145_binary_tree_postorder_traversal;
---
+++ src/problem/p0100_same_tree.rs
@@ -0,0 +1,54 @@
+// 0100. Same Tree
+// https://leetcode.cn/problems/same-tree/
+
+#[allow(dead_code)]
+pub struct Solution {}
+
+use crate::data_structure::tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(p_node), Some(q_node)) => {
+                // node val 相同
+                let is_val_same = { p_node.borrow().val == q_node.borrow().val };
+                // 递归 -> 左子树相同
+                let is_left_same = {
+                    Solution::is_same_tree(
+                        p_node.borrow().left.clone(),
+                        q_node.borrow().left.clone(),
+                    )
+                };
+                // 递归 -> 右子树相同
+                let is_right_same = {
+                    Solution::is_same_tree(
+                        p_node.borrow().right.clone(),
+                        q_node.borrow().right.clone(),
+                    )
+                };
+                is_val_same && is_left_same && is_right_same
+            }
+            _ => false,
+        }
+    }
+}
+
+// unit test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_0100() {
+        assert!(Solution::is_same_tree(tree![1, 2, 3], tree![1, 2, 3]));
+        assert!(!Solution::is_same_tree(tree![1, 2], tree![1, null, 2]));
+        assert!(!Solution::is_same_tree(tree![1, 2, 1], tree![1, 1, 2]),)
+    }
+}

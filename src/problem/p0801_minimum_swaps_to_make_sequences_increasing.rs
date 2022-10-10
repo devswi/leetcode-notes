0a2eba4568cd184515982a474e3653b095c1afe7
shiwei93 <stayfocusjs@gmail.com>
2022-10-10 21:26:16 +0800

    [Add] 0801 finished

--- src/problem/p0801_minimum_swaps_to_make_sequences_increasing.rs
+++ src/problem/p0801_minimum_swaps_to_make_sequences_increasing.rs
@@ -1,13 +1,39 @@
 // 801. Minimum Swaps To Make Sequences Increasing
 // https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing/
 
+use std::vec;
+
 #[allow(dead_code)]
 pub struct Solution {}
 
 #[allow(dead_code)]
 impl Solution {
     pub fn min_swap(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-        unimplemented!()
+        use std::cmp::min;
+        let len = nums1.len();
+        let mut dp = vec![vec![len; 2]; len];
+        // dp[i][0] 不交换
+        // dp[i][1] 交换
+        dp[0][0] = 0;
+        dp[0][1] = 1;
+        println!("{:?}", dp);
+        for i in 1..len {
+            if nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1] {
+                // 顺序满足条件 则
+                // 1. i 和 i - 1 都不交换
+                dp[i][0] = dp[i - 1][0];
+                // 2. i 和 i - 1 都交换
+                dp[i][1] = dp[i - 1][1] + 1;
+            }
+            if nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1] {
+                // 交叉满足条件，则
+                // 1. i 不交换，i - 1 交换
+                dp[i][0] = min(dp[i][0], dp[i - 1][1]);
+                // 2. i 交换，i - 1 不交换
+                dp[i][1] = min(dp[i][1], dp[i - 1][0] + 1);
+            }
+        }
+        min(dp[len - 1][0], dp[len - 1][1]) as i32
     }
 }
 

a84be19186289bbd7ce4f6a833d3ae6bef093814
shiwei93 <stayfocusjs@gmail.com>
2022-10-17 21:57:34 +0800

    [Add] 0904 finished

--- src/problem/p0904_fruit_into_baskets.rs
+++ src/problem/p0904_fruit_into_baskets.rs
@@ -6,16 +6,28 @@ pub struct Solution {}
 
 #[allow(dead_code)]
 impl Solution {
-    pub fn min_add_to_make_valid(s: String) -> i32 {
-        let mut res = vec![];
-        for ch in s.chars() {
-            if ch == ')' && !res.is_empty() && *res.last().unwrap() == '(' {
-                res.pop();
-            } else {
-                res.push(ch);
+    pub fn total_fruit(fruits: Vec<i32>) -> i32 {
+        let mut res = 0;
+        let len = fruits.len();
+        // 统计水果数量
+        let mut count = vec![0; len];
+        let mut total = 0;
+        let mut j = 0;
+        for (i, f) in fruits.iter().enumerate() {
+            count[*f as usize] += 1;
+            if count[*f as usize] == 1 {
+                total += 1;
             }
+            while total > 2 {
+                count[fruits[j] as usize] -= 1;
+                if count[fruits[j] as usize] == 0 {
+                    total -= 1;
+                }
+                j += 1;
+            }
+            res = std::cmp::max(res, i - j + 1);
         }
-        res.len() as i32
+        res as i32
     }
 }
 
@@ -26,7 +38,12 @@ mod tests {
 
     #[test]
     fn test_0904() {
-        assert_eq!(Solution::min_add_to_make_valid(String::from("())")), 1);
-        assert_eq!(Solution::min_add_to_make_valid(String::from("(((")), 3);
+        assert_eq!(Solution::total_fruit(vec![1, 2, 1]), 3);
+        assert_eq!(Solution::total_fruit(vec![0, 1, 2, 2]), 3);
+        assert_eq!(Solution::total_fruit(vec![1, 2, 3, 2, 2]), 4);
+        assert_eq!(
+            Solution::total_fruit(vec![3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]),
+            5
+        );
     }
 }

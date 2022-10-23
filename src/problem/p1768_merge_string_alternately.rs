462d93533a0090eb027ec44d3e6e59e1b3c6fdd9
shiwei93 <stayfocusjs@gmail.com>
2022-10-23 09:58:56 +0800

    [Add] 1768 finished

--- src/problem/p1768_merge_string_alternately.rs
+++ src/problem/p1768_merge_string_alternately.rs
@@ -7,7 +7,27 @@ pub struct Solution {}
 #[allow(dead_code)]
 impl Solution {
     pub fn merge_alternately(word1: String, word2: String) -> String {
-        unimplemented!()
+        let word1_len = word1.len();
+        let word2_len = word2.len();
+        let word1 = word1.chars().collect::<Vec<char>>();
+        let word2 = word2.chars().collect::<Vec<char>>();
+        let mut ans = vec!["".to_string(); word1_len + word2_len];
+        // 双指针
+        let mut i = 0;
+        let mut j = 0;
+
+        while i < word1_len || j < word2_len {
+            if let Some(c) = word1.get(i) {
+                ans.push(c.to_string());
+                i += 1;
+            }
+            if let Some(c) = word2.get(j) {
+                ans.push(c.to_string());
+                j += 1;
+            }
+        }
+
+        ans.into_iter().collect()
     }
 }
 

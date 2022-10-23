793584f16bd1738e63c0ce48a02b8053d0f52a7e
shiwei93 <stayfocusjs@gmail.com>
2022-10-23 15:53:49 +0800

    [Add] 0098 finished

--- src/problem/p0098_validate_binary_search_tree.rs
+++ src/problem/p0098_validate_binary_search_tree.rs
@@ -12,8 +12,19 @@ type Node = Rc<RefCell<TreeNode>>;
 
 #[allow(dead_code)]
 impl Solution {
-    pub fn is_valid_bst(_root: Option<Node>) -> bool {
-        unimplemented!()
+    pub fn is_valid_bst(root: Option<Node>) -> bool {
+        Solution::helper(root.as_ref(), i32::MIN, i32::MAX)
+    }
+
+    fn helper(root: Option<&Node>, lower: i32, upper: i32) -> bool {
+        if let Some(node) = root {
+            if node.borrow().val < lower || node.borrow().val > upper {
+                return false;
+            }
+            return Solution::helper(node.borrow().left.as_ref(), lower, node.borrow().val)
+                && Solution::helper(node.borrow().right.as_ref(), node.borrow().val, upper);
+        }
+        true
     }
 }
 

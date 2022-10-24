7154054fe129f8a791d3c5fd626d309070f70ad8
shiwei93 <stayfocusjs@gmail.com>
2022-10-24 10:44:43 +0800

    [Add] 0915 finished

--- src/problem/p0915_partition_array_into_disjoint_intervals.rs
+++ src/problem/p0915_partition_array_into_disjoint_intervals.rs
@@ -6,8 +6,24 @@ pub struct Solution {}
 
 #[allow(dead_code)]
 impl Solution {
-    pub fn partition_disjoint(_nums: Vec<i32>) -> i32 {
-        unimplemented!()
+    pub fn partition_disjoint(nums: Vec<i32>) -> i32 {
+        use std::cmp::max;
+        if nums.is_empty() {
+            return -1;
+        }
+        let mut pos = 0;
+        // 0~pos 的最大值
+        let mut max_pos = nums[0];
+        // 0~i 中的最大值
+        let mut max_i = nums[0];
+        for num in nums[1..].iter() {
+            max_i = max(max_i, *num);
+            if max_i <= max_pos {
+                pos += 1;
+                max_pos = max(*num, max_pos);
+            }
+        }
+        pos + 1
     }
 }
 

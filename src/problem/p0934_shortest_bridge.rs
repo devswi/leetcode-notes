40b047ca142c8db1f1730aab7a58cd032b2209ba
shiwei93 <stayfocusjs@gmail.com>
2022-10-25 21:48:06 +0800

    [Add] 0934 finished

--- src/problem/p0934_shortest_bridge.rs
+++ src/problem/p0934_shortest_bridge.rs
@@ -4,10 +4,73 @@
 #[allow(dead_code)]
 pub struct Solution {}
 
+use std::collections::VecDeque;
+
 #[allow(dead_code)]
 impl Solution {
-    pub fn shortest_bridge(_grid: Vec<Vec<i32>>) -> i32 {
-        unimplemented!()
+    pub fn shortest_bridge(grid: Vec<Vec<i32>>) -> i32 {
+        let len = grid.len();
+        let mut grid = grid;
+        let mut queue = VecDeque::new();
+        'exit: for i in 0..len {
+            for j in 0..len {
+                if grid[i][j] == 1 {
+                    Solution::dfs(&mut grid, &mut queue, i, j);
+                    break 'exit;
+                }
+            }
+        }
+
+        let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+        while let Some((i, j, d)) = queue.pop_front() {
+            match grid[i][j] {
+                1 => {
+                    return d;
+                }
+                0 | 2 => {
+                    grid[i][j] = 3;
+
+                    for &(dx, dy) in &dirs {
+                        let x = (i as i32 + dx) as usize;
+                        let y = (j as i32 + dy) as usize;
+
+                        if x < len && y < len {
+                            queue.push_back((x, y, d + 1))
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        0
+    }
+
+    fn dfs(
+        grid: &mut Vec<Vec<i32>>,
+        queue: &mut VecDeque<(usize, usize, i32)>,
+        x: usize,
+        y: usize,
+    ) {
+        let len = grid.len();
+        if x >= len || y >= len || grid[x][y] == 2 {
+            return;
+        }
+        if grid[x][y] == 0 {
+            queue.push_back((x, y, 0));
+            return;
+        }
+        // 标记已访问的土地
+        grid[x][y] = 2;
+        if x > 0 {
+            Solution::dfs(grid, queue, x - 1, y);
+        }
+        Solution::dfs(grid, queue, x + 1, y);
+        if y > 0 {
+            Solution::dfs(grid, queue, x, y - 1);
+        }
+        Solution::dfs(grid, queue, x, y + 1);
     }
 }
 
